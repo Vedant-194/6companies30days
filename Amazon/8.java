dad6631890efcb4ea44020b17d45c4b6598733f3
Vedant Bhatt <84610560+Vedant-194@users.noreply.github.com>
2022-01-27 16:20:30 +0530

    Create 8.java

---
+++ Amazon/8.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+
+class GFG {
+	public static void main (String[] args) {
+		
+		//taking input using Scanner class
+		Scanner sc = new Scanner(System.in);
+		
+		//taking total testcases
+		int t = sc.nextInt();
+		
+		while(t-- > 0){
+		    
+		    //taking total number of stairs
+		    int m = sc.nextInt();
+		    
+		    //creating an object of class DynamicProgramming
+		    Solution obj = new Solution();
+		    
+		    //calling method of class countWays()
+		    //of class DynamicProgramming
+		    System.out.println(obj.countWays(m));
+		    
+		}
+		
+	}
+}
+// } Driver Code Ends
+
+
+
+class Solution
+{
+    Long countWays(int m)
+    {
+return (long)(m/2+1);
+    }    
+}
